aad287aeb9a9499128c0a0453cac259f0025c456
kamilla <kamilla.melikava@gmail.com>
2024-12-13 09:57:20 +0300

    Add project files.

---
+++ MasterTool_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MasterTool_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
